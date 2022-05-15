f0e332c9a94d2478109acdb663850e9bffefce49
rajat-2002 <104300096+rajat-2002@users.noreply.github.com>
2022-05-15 22:37:55 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,36 @@
+
+@@ -0,0 +1,36 @@
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+
+ENTER YOUR NAMErajat kumar
+ENTER YOUR SID21102115
+ENTER YOUR DEPARTMENTcivil
+ENTER YOUR CGPA 7.0
+Hey rajat kumar here
+My SID is 21102115
+I am from civil and my CGPA is 7.0
+
+
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+
+
+ENTER A STRINGname
+Yes
+
+
+ENTER FIRST SIDE OF TRIANGLE5
+ENTER SECOND SIDE OF TRIANGLE6
+ENTER THIRD SIDE OF TRIANGLE8
+Yes
+
+4
